456a31643a6fda1e3a2158a09eb767f31f5cf084
Alexander <rasha@oregonstate.edu>
2020-11-28 16:43:28 -0800

    Customer updating funtionality now works

--- customer.js
+++ customer.js
@@ -1,3 +1,5 @@
+const { pool } = require('./dbcon');
+
 module.exports = function(){
     var express = require('express');
     var router = express.Router();
@@ -11,6 +13,19 @@ module.exports = function(){
             context.customer = results;
             complete();
         });
+    }
+    function getCustomerById(res, mysql, context, id, complete){
+        var sql = "SELECT Customers.customer_id as id, first_name, last_name, email, password, address_1, address_2, city, state, zip_code FROM Customers WHERE customer_id = ?";
+        var inserts = [id];
+        mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.customer = results[0];
+            complete();
+        });
+
     }
 
     /* Find customer whose email starts with a given string in the req */
@@ -63,6 +78,23 @@ module.exports = function(){
         }
     });
 
+    /* Used for updating a chosen customer */
+
+    router.get('/:id', function(req, res){
+        callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["updatecustomer.js"];
+        var mysql = req.app.get('mysql');
+        getCustomerById(res, mysql, context, req.params.id, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 1){
+                console.log(context);
+                res.render('update-customer', context);
+            }
+        }
+    });
+
     /* Adds a customer, redirects to the people page after adding */
 
     router.post('/', function(req, res){
@@ -76,6 +108,25 @@ module.exports = function(){
                 res.end();
             }else{
                 res.redirect('/customer');
+            }
+        });
+    });
+
+    /* The URI that updates data is sent to so that a customer is updated */
+    router.put('/:id', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "UPDATE Customers SET first_name = ?, last_name = ?, email = ?, password = ?, address_1 = ?, address_2 = ?, city = ?, state = ?, zip_code = ? WHERE customer_id = ?";
+        var inserts = [req.body.first_name, req.body.last_name, req.body.email, req.body.password, req.body.address_1, req.body.address_2, req.body.city, req.body.state, req.body.zip_code, req.params.id];
+        sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                console.log("There was an issue updating the selected customer.")
+                console.log(error)
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            else{
+                res.status(200);
+                res.end();
             }
         });
     });
--- dbcon.js
+++ dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_lamcal',
-  password        : '5142',
-  database        : 'cs340_lamcal'
+  user            : 'cs340_rasha',
+  password        : '6199',
+  database        : 'cs340_rasha'
 });
 module.exports.pool = pool;

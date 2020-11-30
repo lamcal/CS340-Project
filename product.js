3c1f6f73fe46ead01342cb34930d9e29b775d18e
Alexander <rasha@oregonstate.edu>
2020-11-29 17:49:08 -0800

    Products updating functionality implemented

--- product.js
+++ product.js
@@ -13,6 +13,19 @@ module.exports = function(){
         });
     }
 
+    /*Get a specific product based on a given order_id */
+    function getProductById(res, mysql, context, id, complete){
+        var sql = "SELECT Products.product_id as id, supplier_id, product_name, product_stock, price FROM Products WHERE product_id = ?";
+        var inserts = [id];
+        mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.product = results[0];
+            complete();
+        });
+    }
     /* Display all product */
 
     router.get('/', function(req, res){
@@ -25,6 +38,23 @@ module.exports = function(){
             callbackCount++;
             if(callbackCount >= 1){
                 res.render('product', context);
+            }
+        }
+    });
+
+    /* Used for updating a chosen product */
+
+    router.get('/:id', function(req, res){
+        callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["updateproduct.js"];
+        var mysql = req.app.get('mysql');
+        getProductById(res, mysql, context, req.params.id, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 1){
+                console.log(context);
+                res.render('update-product', context);
             }
         }
     });
@@ -42,6 +72,25 @@ module.exports = function(){
                 res.end();
             }else{
                 res.redirect('/product');
+            }
+        });
+    });
+
+    /* The URI that updates what data is sent to so that a chosen products is updated */
+    router.put('/:id', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "UPDATE Products SET supplier_id = ?, product_name = ?, product_stock = ?, price = ? WHERE product_id = ?";
+        var inserts = [req.body.supplier_id, req.body.product_name, req.body.product_stock, req.body.price, req.params.id];
+        sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                console.log("There was an issue updating the selected product.")
+                console.log(error)
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            else{
+                res.status(200);
+                res.end();
             }
         });
     });
